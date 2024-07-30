8d1ff7c8c267b3805aa2c03d23d88f93995bd8cd
AbhijeetKumarMonash <141550630+AbhijeetKumarMonash@users.noreply.github.com>
2024-07-30 22:15:46 +1000

    week 1

---
+++ fit5032-w1/src/components/MyName.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+     <h1>{{ message }}</h1>   
+      <h1>{{ name }}</h1>
+    </div>
+  </template>
+  
+  <script setup>
+  const message = "Efolio Task 1.2 Output:";
+  const name = "Abhijeet Kumar"; 
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
